64ae7e572211ecbf8884fb0db26816f7ed39be6d
amrekano102-pixel <amrekano102@gmail.com>
2026-07-28 00:26:10 +0300

    SW: minimal, just clear old caches

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-const CACHE_NAME = 'borooz-v3';
-const urlsToCache = [
-  'index.html',
-  'css/style.css',
-  'js/app.js',
-  'js/database.js',
-  'js/ads.js',
-  'manifest.json',
-];
-
 self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
@@ -15,20 +5,10 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(name) {
-          return caches.delete(name);
-        })
-      );
+      return Promise.all(cacheNames.map(function(name) {
+        return caches.delete(name);
+      }));
     })
   );
   self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
-  );
 });
